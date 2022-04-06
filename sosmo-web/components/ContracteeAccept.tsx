5e39b9a92f6a826d5c87c8b3e1d8545ad0d3bdea
michael <forgetscode@gmail.com>
2022-04-06 01:14:26 -0700

    contract hooked up for init, cancel and open!

--- sosmo-web/components/ContracteeAccept.tsx
+++ sosmo-web/components/ContracteeAccept.tsx
@@ -1,4 +1,9 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+import * as anchor from '@project-serum/anchor';
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 import { useUpdateStateMutation } from "../generated/graphql";
+import  idl from '../target/idl/agreement.json';
+import { Agreement } from '../target/types/agreement';
 
 interface ContractProps {
     postid:number,
@@ -9,9 +14,32 @@ interface ContractProps {
 
 export const ContracteeAccept = ( props:ContractProps ) => {
     const [ updateState ] = useUpdateStateMutation();
+    const wallet = useWallet();
+    const programID = new PublicKey(idl.metadata.address);              
+    const network = clusterApiUrl('devnet');
+    const connection = new Connection(network, "processed");
+    const provider = new anchor.Provider(connection, wallet, "processed");
+    const program = new anchor.Program<Agreement>(idl, programID, provider);
+
+    let data = async () =>{
+        const buffer = new PublicKey(props.discriminator);
+        const contractor = new PublicKey(props.contractor);
+
+        const [contractPDA, _ ] = await PublicKey
+        .findProgramAddress(
+          [
+            anchor.utils.bytes.utf8.encode("contract_acc"),
+            contractor.toBuffer(),
+            buffer.toBuffer(),
+          ],
+          programID
+        );
+        return data;
+    }
+    
     return (
         <div className='flex flex-col'>
-            <p className='ml-3 text-green-600 bg-slate-700 p-4 rounded-md'>DATA that is in the contract</p>
+            <p className='ml-3 text-green-600 bg-slate-700 p-4 rounded-md'>{data}</p>
             <button className="mr-auto text-white bg-emerald-700 ml-3 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800 mt-2"
                 onClick={async () => {
                     {
